0d4359847e3c656716337c1629d3c3d4b9c5c184
weibing.liu <weibing.liu@qunar.com>
2018-02-05 22:54:54 +0800

    'test'

--- Config.py
+++ Config.py
@@ -5,15 +5,15 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_EKY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASK_MAIL_SUBJECT_PREFIX = '[FLASKY]'
-    FLASK_MAIL_SENDER = '13598508974@126.com'
+    FLASK_MAIL_SENDER = os.environ.get('MAIL_USER_NAME') + '@139.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASK_POSTS_PER_PAGE = 10
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAIL_SERVER = 'smtp.139.com'
     MAIL_PORT = 25
     MAIL_USE_TLS = True
-    MAIL_USER_NAME = '13598508974'
-    MAIL_PASSWORD = 'LIU1028140200'
+    MAIL_USER_NAME = os.environ.get('MAIL_USER_NAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FATE_PER_PAGE = 30
 
 
